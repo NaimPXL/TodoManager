83f5152ddee819799cb28d0ddb2cf227cd8ba7ac
NaïmFarjiPXL <naim.farji@student.pxl.be>
2026-03-08 17:32:41 +0100

    Added testing on Domain and Application Layer.

---
+++ TodoManager.Application.Tests/TodoServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoManager.Application.Services;
+
+namespace TodoManager.Application.Tests
+{
+    public class TodoServiceTests
+    {
+        [Fact]
+        public void AddTodo_DuplicateTitle_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            TodoService service = new TodoService();
+
+            service.AddTodo("Buy milk", null, DateTime.Today.AddDays(1));
+
+            // Act + Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                service.AddTodo("  BUY MILK  ", "Duplicate", DateTime.Today.AddDays(2));
+            });
+        }
+
+        [Fact]
+        public void DeleteTodo_PastDueDate_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            TodoService service = new TodoService();
+
+            service.AddTodo("Old task", null, DateTime.Today.AddDays(-1));
+
+            var todos = service.GetTodos();
+            var todoToDelete = todos[0];
+
+            // Act + Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                service.DeleteTodo(todoToDelete);
+            });
+        }
+
+        [Fact]
+        public void DeleteTodo_CompletedTodo_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            TodoService service = new TodoService();
+
+            service.AddTodo("Finish assignment", null, DateTime.Today.AddDays(2));
+
+            var todos = service.GetTodos();
+            var todo = todos[0];
+
+            service.CompleteTodo(todo);
+
+            // Act + Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                service.DeleteTodo(todo);
+            });
+        }
+
+        [Fact]
+        public void CompleteTodo_SetsCompletedAt()
+        {
+            // Arrange
+            TodoService service = new TodoService();
+
+            service.AddTodo("Read chapter", null, DateTime.Today.AddDays(3));
+
+            var todos = service.GetTodos();
+            var todo = todos[0];
+
+            // Act
+            service.CompleteTodo(todo);
+
+            // Assert
+            Assert.True(todo.IsCompleted);
+            Assert.NotNull(todo.CompletedAt);
+        }
+    }
+}
---
+++ TodoManager.Domain.Tests/TodoItemTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoManager.Domain.Models;
+
+namespace TodoManager.Domain.Tests
+{
+    public class TodoItemTests
+    {
+        [Fact]
+        public void Constructor_EmptyTitle_ThrowsArgumentException()
+        {
+            // Arrange
+            string title = "";
+            string? description = null;
+            DateTime dueDate = DateTime.Today.AddDays(1);
+
+            // Act + Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                TodoItem item = new TodoItem(title, description, dueDate);
+            });
+        }
+
+        [Fact]
+        public void Constructor_WhitespaceTitle_ThrowsArgumentException()
+        {
+            // Arrange
+            string title = "   ";
+            string? description = "Some description";
+            DateTime dueDate = DateTime.Today.AddDays(1);
+
+            // Act + Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                TodoItem item = new TodoItem(title, description, dueDate);
+            });
+        }
+
+        [Fact]
+        public void Constructor_NullDescription_DoesNotThrow()
+        {
+            // Arrange
+            string title = "Buy milk";
+            string? description = null;
+            DateTime dueDate = DateTime.Today.AddDays(1);
+
+            // Act
+            TodoItem item = new TodoItem(title, description, dueDate);
+
+            // Assert
+            Assert.Null(item.Description);
+            Assert.Equal("Buy milk", item.Title);
+        }
+
+        [Fact]
+        public void MarkAsCompleted_SetsIsCompletedAndCompletedAt()
+        {
+            // Arrange
+            TodoItem item = new TodoItem("Study C#", null, DateTime.Today.AddDays(3));
+
+            // Act
+            item.MarkAsCompleted();
+
+            // Assert
+            Assert.True(item.IsCompleted);
+            Assert.NotNull(item.CompletedAt);
+        }
+    }
+}
